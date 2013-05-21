dff94c7f211ce8c17ca59dcbb74d82e72cdc4746
sgautam <sadiksha.gautam@rwth-aachen.de>
2013-05-21 20:30:46 +0200

    Authentication started

--- index.php
+++ index.php
@@ -1,5 +1,32 @@
-<?php 
-echo "Abc";
+<html>
+<body>
+<script src="js/jquery.js"></script>
+<form id="loginForm" method="post">
+Username: <input id="usernameInput" type="text">
+Password: <input id="passwordInput" type="text">
+<input type="submit">
+</form>
+<script>
 
 
-?>
+$(document).ready(function(){
+  var username = $('#usernameInput').val();
+  var password = $('#passwordInput').val();
+  $('#loginForm').submit(function() {
+  console.log(username);
+   $.ajax
+   ({
+     url: "http://merkur.informatik.rwth-aachen.de/bscw/bscw.cgi/3496028",
+     dataType: 'json',
+     data: {'username': 'gamification', 'password': 'gami123++'},
+     success: function (){
+      alert('Thanks for login!'); 
+     }
+  });
+  return false;
+  });
+ 
+  }); 
+</script>
+</body>
+</html>
