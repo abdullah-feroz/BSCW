b94efdb35d7c953fb02363beb624f7b7f14dc931
abdullah-feroz <abdullah_fer@hotmail.com>
2013-06-15 03:24:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,38 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+  <link rel="stylesheet" href="css/default.css" />
+	<script src="js/jquery-1.2.6.pack.js" type="text/javascript"></script>
+	<script src="js/myScript.js" type="text/javascript"></script>
+</head>
 <body>
-<script src="js/jquery.js"></script>
-<form id="loginForm" method="post" action="authentication.php">
-Username: <input id="usernameInput" type="text" name="username">
-Password: <input id="passwordInput" type="password" name="password">
-<input type="submit">
-</form>
+<?php require_once "includes/functions.php"; ?>
+<div id="wrap">
+
+<ul id="nav">
+<li><a href="#content_1" class="selected">NETTUTS</a></li>
+<li><a href="#content_2">ThemeForest</a></li>
+<li><a href="#content_3">Screencasts</a></li>
+</ul>
+
+	<div id="mainContent">
+
+		<div id="content_1">
+			<?php getFeed("http://feedproxy.google.com/nettuts"); ?>
+		</div><!--end content 1-->
+
+		<div id="content_2">
+			<?php getFeed("http://feedproxy.google.com/themeforest"); ?>
+		</div><!--end content 2-->
+
+		<div id="content_3">
+			<?php getFeed("http://feeds.feedburner.com/NETTUTSVideos"); ?>
+		</div><!--end content 3-->
+
+	</div><!--end main content -->
+
+</div><!--end wrap-->
 </body>
 </html>
